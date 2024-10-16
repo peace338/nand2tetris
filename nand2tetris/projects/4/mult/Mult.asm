a6110efd3bbd2fb39d2da5a8b285fca3ea0da245
peace338 <yjpu1108@gmail.com>
2024-10-17 00:40:25 +0900

    implements Mult but it takes time too long to compute

--- nand2tetris/projects/4/mult/Mult.asm
+++ nand2tetris/projects/4/mult/Mult.asm
@@ -7,4 +7,33 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+
+//add R0 repetitive nth. n is equal R1
+
+    //i=0
+    @0
+    D=A
+    @i
+    M=D
+
+(LOOP)
+    @R0
+    D=M
+
+    @R2
+    M=D+M
+
+    @i
+    M=M+1
+    D=M
+    //R1-i
+    @R1
+    D=M-D
+
+    //goto (LOOP)
+    @LOOP
+    D;JLE
+
+(END)
+    @END
+    0;JMP
